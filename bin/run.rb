3d13ce278e4396e2d07c3b0ece787bab24ada230
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-09 14:48:35 -0400

    added pry to runner

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,5 @@
-require_relative '../config/environment'
+require_relative "../config/environment"
 
-
-
+require "pry"
+Binding.pry
 puts "HELLO WORLD"
--- lib/models/drawing.rb
+++ lib/models/drawing.rb
@@ -1,6 +1,5 @@
 class Drawing < ActiveRecord::Base
   # has_many :user_drawings,
   # has_many :users, through :user_drawing,
-  # has_many :shapes
-
+  has_many :shapes
 end
