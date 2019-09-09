d798263aa822fa1daacb3b345fa63f8b7e541399
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-09 14:41:58 -0400

    setup database, setup the drawings and shapes tables.

--- db/migrate/20190909183119_create_drawings_table.rb
+++ db/migrate/20190909183119_create_drawings_table.rb
@@ -1,4 +1,8 @@
 class CreateDrawingsTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :drawings do |t|
+      t.string :title
+      t.timestamps
+    end
   end
 end
--- db/migrate/20190909183125_create_shapes_table.rb
+++ db/migrate/20190909183125_create_shapes_table.rb
@@ -1,4 +1,11 @@
 class CreateShapesTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :shapes do |t|
+      t.integer :drawing_id
+      t.integer :x
+      t.integer :y
+      t.string :color
+      t.timestamps
+    end
   end
 end
