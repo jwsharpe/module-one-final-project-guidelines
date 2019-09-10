5cf11d6406c997d6ca96b6ccafcc2c6d92d37a5d
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-10 08:59:34 -0400

    attached title functionality to drawing window

--- lib/models/drawing.rb
+++ lib/models/drawing.rb
@@ -1,9 +1,8 @@
 class Drawing < ActiveRecord::Base
-  has_many :shapes
-  has_many :users, through: :user_drawing 
-  
+  has_many :shapes, dependent: :destroy
+  has_many :users, through: :user_drawing
+
   def open
     Window.open(self)
   end
-  
 end
