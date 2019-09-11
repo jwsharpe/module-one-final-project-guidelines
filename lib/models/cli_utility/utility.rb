4001c057704b9bd4b602faa5ef16463ec07b37d6
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-11 09:55:12 -0400

    added collaborative functionality

--- lib/models/cli_utility/utility.rb
+++ lib/models/cli_utility/utility.rb
@@ -1,15 +1,20 @@
 
-def print_list(list) #helper method to print list
+def print_greeting
+  puts "Welcome To Drawing Application"
+  puts "==============================="
+end
+
+def print_attribute_list(list, attr) #helper method to print list
   list.each_with_index do |element, index|
-    puts "#{index + 1}: #{element.title}"
+    puts "#{index + 1}: #{element.send(attr)}"
   end
 end
 
-def privacy_option
+def print_privacy_options
   puts "Choose option:"
-  puts "1. Private Project."
-  puts "2. Public Project."
-  puts "3. Collaborative"
+  puts "1. Private Project"
+  puts "2. Public Project"
+  puts "3. Collab Project"
 end
 
 def prompt_new_project(privacy_setting)
