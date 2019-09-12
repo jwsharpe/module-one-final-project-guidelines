4785927bccd6da07e6396b9ae48d311bd02e12b3
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-12 11:03:18 -0400

    finished refactor of project_scene

--- lib/controller/scenes/project_scene.rb
+++ lib/controller/scenes/project_scene.rb
@@ -4,31 +4,20 @@ class ProjectScene < Scene
   def run
     CliRenderer.print_privacy_header(session.privacy_setting.upcase)
 
-    scene_handle_
-    if session.drawings
-      drawings = session.drawings.where(private: session.privacy_setting)
-      CliRenderer.print_attribute_list(drawings, :title)
-    else
-      CliRenderer.print_header("No drawing exist! Create New Or Choose Public")
-    end
-
-    CliRenderer.print_create_drawing unless is_collaborative?
+    scene_handle_greeting
     program_choice = gets.chomp
 
-    if program_choice == "+" && !is_collaborative?
+    case program_choice
+    when "b"
+      self.next_scene("privacy_scene")
+    when "e"
+      self.next_scene("exit_scene")
+    when drawings[program_choice.to_i - 1]
+      scene_save_drawing_to_session_data
+      self.next_scene("setting_scene")
+    when "+" && !is_collaborative?
       scene_show_create_new_drawing
       self.next_scene("setting_scene")
-    elsif (program_choice.to_i > 0)
-      if (drawings[program_choice.to_i - 1])
-        scene_save_drawing_to_session_data
-        self.next_scene("setting_scene")
-      else
-        run
-      end
-    elsif program_choice == "b"
-      self.next_scene("privacy_scene")
-    elsif program_choice == "e"
-      self.next_scene("exit_scene")
     else
       CliRenderer.print_invalid_input
       run
@@ -51,4 +40,15 @@ end
 
 def scene_save_drawing_to_session_data
   session.drawing = drawings[program_choice.to_i - 1]
+end
+
+def scene_handle_greeting
+  if session.drawings
+    drawings = session.drawings.where(private: session.privacy_setting)
+    CliRenderer.print_attribute_list(drawings, :title)
+  else
+    CliRenderer.print_header("No drawing exist! Create New Or Choose Public")
+  end
+
+  CliRenderer.print_create_drawing unless is_collaborative?
 end
